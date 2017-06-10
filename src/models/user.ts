58a5849c0fa479e37e237225b6761e74598a7c90
Shine <auttapon.shine@gmail.com>
2017-06-10 22:58:59 +0700

    Fixed bug

---
+++ src/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public id: number,
+        public email: string,
+        public name: string,
+        public created_at: string,
+        public updated_at: string
+    ) {}
+}
